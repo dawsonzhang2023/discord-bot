ed8f76fd7ffc94b0d0d2c1b2b77db42718402b25
dawsonzhang2023 <dawsonzhang@outlook.com>
2023-05-31 13:04:26 -0400

    add async method

--- src/handlers/wtwitter.js
+++ src/handlers/wtwitter.js
@@ -5,7 +5,7 @@ const twitter_template = require("../templates/twitter-t");
  * @param {OpenAIApi} openai
  */
 
-module.exports = (action, openai) => {
+module.exports = async (action, openai) => {
   const shareInfo = action.options.get("share_information").value;
   const target = action.options.get("target").value;
   const brand = action.options.get("brand").value;
@@ -14,6 +14,18 @@ module.exports = (action, openai) => {
   console.log(`user input ${shareInfo} ,  ${target} ,  ${brand} , ${experts} `);
 
   const prompt = twitter_template(shareInfo, target, brand, experts);
+
+  await action.deferReply(true);
+  let gptReply = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${prompt}`,
+    temperature: 0.9,
+    max_tokens: 500,
+    stop: ["ChatGPT"],
+  });
+  //action.deferReply(`${gptReply.data.choices[0].text}`);
+  // action.editReply(`${gptReply.data.choices[0].text}`);
+  await action.followUp(`${gptReply.data.choices[0].text}`);
 
   action.reply(prompt);
 };
